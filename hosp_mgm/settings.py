7d7ccc52ebda2f6c133bbceba6e867e8e9efc9a7
jignasa-dcosta <jignasadcosta@gmail.com>
2025-03-09 20:31:33 +0530

    Add WhiteNoise middleware for static file handling

--- hosp_mgm/settings.py
+++ hosp_mgm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'appointment',
     'django_extensions',
 ]
@@ -48,6 +49,7 @@ GRAPH_MODELS ={
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
