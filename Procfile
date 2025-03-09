6f27577bb11f60bd4a655fc5dd51ef035d1dea5a
jignasa-dcosta <jignasadcosta@gmail.com>
2025-03-09 18:53:15 +0530

    Include Procfile and SQLite database

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hosp_mgm.wsgi --log-file -
--- hosp_mgm/settings.py
+++ hosp_mgm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@*0-h*e_a)*y02kal$smfrd#_0^c9#qne+xp=77i)6ujlakj7c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
