29f260d3b2e431bf60f1cc2353d391ad3cf04cb8
jignasa-dcosta <jignasadcosta@gmail.com>
2025-03-09 23:05:30 +0530

    Add Cloudinary and Cloudinary Storage to installed apps

--- hosp_mgm/settings.py
+++ hosp_mgm/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'cloudinary',
+    'cloudinary_storage',
     'appointment',
     'django_extensions',
 ]
